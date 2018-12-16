1a1b1dd1be5e5a6f66c05a998ef4af9ef390e528
lancellottidaniel <45920154+lancellottidaniel@users.noreply.github.com>
2018-12-16 15:10:30 -0200

    Add files via upload

---
+++ Pessoa/Pessoa/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pessoa
+{
+    public class clsPessoa
+    {
+        private static int codigo;
+        private static String nome;
+        private static String sobrenome;
+        private static DateTime data_nasc;
+        private static String sexo;
+        private static String email;
+        private static Boolean ativo;
+
+        public static void setCodigo(int _codigo) { codigo = _codigo; }
+        public static void setNome(String _nome) { nome = _nome; }
+        public static void setSobrenome(String _sobrenome) { sobrenome = _sobrenome; }
+        public static void setDataNasc(DateTime _datanasc) { data_nasc = _datanasc; }
+        public static void setSexo(String _sexo) { sexo = _sexo; }
+        public static void setEmail(String _email) { email = _email; }
+        public static void setAtivo(Boolean _ativo) { ativo = _ativo; }
+
+        public static int getCodigo() { return codigo; }
+        public static String getNome() { return nome; }
+        public static String getSobrenome() { return sobrenome; }
+        public static DateTime getDataNasc() { return data_nasc; }
+        public static String getSexo() { return sexo; }
+        public static String getEmail() { return email; }
+        public static Boolean getAtivo() { return ativo; }
+    }
+}
---
+++ Produto/Produto/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Produto
+{
+    public class clsProduto
+    {
+        private static int codigo;
+        private static int quantidade;
+        private static String produto;
+
+        public static void setCodigo(int _codigo) { codigo = _codigo; }
+        public static void setQuantidade(int _quantidade) { quantidade = _quantidade; }
+        public static void setProduto(String _produto) { produto = _produto; }
+
+        public static int getCodigo() { return codigo; }
+        public static int getQuantidade() { return quantidade; }
+        public static String getProduto() { return produto; }
+    }
+}
